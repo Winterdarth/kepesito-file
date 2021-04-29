77c981927d8c06a3b445621356c45dc716ef9aee
David Kajdarov <kajdarov.d@gmail.com>
2021-04-29 11:56:58 +0200

    Finish project

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,20 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public class Phonebook {
+
+    public static void exportPhonebook(Map<String, String> contacts, String output) throws FileNotFoundException {
+        if(contacts == null || output == null) {
+            throw new IllegalArgumentException("n must be positive");
+        }
+
+        PrintWriter outputFile = new PrintWriter(output);
+        for(String key :contacts.keySet()) {
+            outputFile.println(key +": "+ contacts.get(key));
+        }
+        outputFile.close();
+    }
+}
